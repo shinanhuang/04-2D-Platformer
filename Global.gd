e0abf65e8c6566aa94cae5ac4fa9055ed56b15dd
shinanhuang <122583606+shinanhuang@users.noreply.github.com>
2023-04-27 19:11:40 -0400

    With a not functioning In Game Menu

--- Global.gd
+++ Global.gd
@@ -6,9 +6,22 @@ var health = 100
 var level = 1
 var max_health = 100
 var max_lives = 5
+var menu = null
 
 func _ready():
-	pass
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("menu"):
+		if menu == null:
+			menu = get_node_or_null("/root/Game/UI/In_Game_Menu")
+		if menu != null:
+			if not menu.visible:
+				get_tree().paused = true
+				menu.show()
+			else:
+				get_tree().paused = false
+				menu.hide()
 
 func increase_score(s):
 	score += s
